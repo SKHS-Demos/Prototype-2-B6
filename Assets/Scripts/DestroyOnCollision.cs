4df2b61e3b0f1915f51a5aebb5301c0994bd8ada
Douglas Urner <dlu@canishe.com>
2019-12-06 11:41:17 -0800

    Test case for failure to destroy objects on collision.
    Per Ali.

---
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // OnTriggerEnter is called on a collision (if isTrigger)
+    private void OnTriggerEnter(Collider other) {
+       Destroy(gameObject);
+       Destroy(other.gameObject);
+    }
+}
